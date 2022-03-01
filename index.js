87c8faba36e53570839fd157aa2e1d34e380c036
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-03-01 09:14:41 -0500

    commit tree 6dfed3264213563f1fdcf5ca42a86087da95702c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(arrayOfStrings, stringWeLookFor){
+    const arrayOfFoundNames = arrayOfStrings.filter((name=>{if(name.toUpperCase() === stringWeLookFor.toUpperCase()){return true;}}) , stringWeLookFor);
+    return arrayOfFoundNames
+}
+
+function fuzzyMatch(arrayOfNames, namesThatStartWithSameLetters){
+
+const arrayOfStringsWithMatchingChar = arrayOfNames.filter((character=>{if (character.startsWith()=== namesThatStartWithSameLetters.startsWith()){return true;}}), namesThatStartWithSameLetters)
+return arrayOfStringsWithMatchingChar
+} 
+
+function matchName(arrayOfDrivers, sameDrivers){
+    const sameNames = arrayOfDrivers.filter((name)=>{if (arrayOfDrivers.name === arrayOfDrivers.name){return true}}, sameDrivers)
+    return sameNames
+}
